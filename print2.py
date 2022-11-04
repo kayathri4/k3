df6bfc048da3758e7be867878edbdbd85c0d8d90
Kayaythri4 <kayathriv05@gmail.com>
2022-11-04 13:34:00 +0530

    new new changedeereter

--- print2.py
+++ print2.py
@@ -1,1 +1,12 @@
-print('hi')
+def my_function(food):
+  for x in food:
+    print(x)
+  print("==========================")
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+fruits = ["apple", "banana", "cherry", "strawberry"]
+my_function(fruits)
+fruits = ["apple", "banana", "cherry", "asd", "dsa"]
+my_function(fruits)
